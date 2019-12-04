8580fcaa552faa5d39abc09179d8c67291e761ff
brect <b.limas93@gmail.com>
2019-12-03 23:04:55 -0300

    1- Implementação inicial da lista de notas 2- Conhecendo o RecyclerView

---
+++ app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.java
@@ -0,0 +1,53 @@
+package br.com.alura.ceep.ui.recyclerview.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import br.com.alura.ceep.R;
+import br.com.alura.ceep.model.Nota;
+import br.com.alura.ceep.ui.activity.ListaNotasActivity;
+
+public class ListaNotasAdapter extends RecyclerView.Adapter {
+
+
+    private List<Nota> notas;
+    private Context context;
+
+    public ListaNotasAdapter(Context context, List<Nota> notas){
+        this.context = context;
+        this.notas = notas;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View viewCriada = LayoutInflater.from(context).inflate(R.layout.item_nota, parent, false);
+        return new NotaViewHolder(viewCriada);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        Nota nota = notas.get(position);
+        TextView titulo = holder.itemView.findViewById(R.id.item_nota_titulo);
+        titulo.setText(nota.getTitulo());
+        TextView descricao = holder.itemView.findViewById(R.id.item_nota_descricao);
+        descricao.setText(nota.getDescricao());
+    }
+
+    @Override
+    public int getItemCount() {
+        return notas.size();
+    }
+
+    class NotaViewHolder extends RecyclerView.ViewHolder{
+
+        public NotaViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
